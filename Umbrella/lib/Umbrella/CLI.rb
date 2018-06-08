bca61dc8ba9ba1392bf5ce80cbf898da96bce8ed
Conor Hamilton <conhamil19@gmail.com>
2018-06-08 05:00:18 +0000

    CLI work

--- Umbrella/lib/Umbrella/CLI.rb
+++ Umbrella/lib/Umbrella/CLI.rb
@@ -3,15 +3,29 @@ class Umbrella::CLI
   def call 
     #need to figure out input for first line.
     puts "To grab umbrella or not"
+    further_prompt
+    choose
+  end 
+  
+    def further_prompt
     puts "Would you like to know more about today's weather(y/n):"
-    input = gets.chomp 
+    input = gets.chomp.downcase
     if input == "y"
-      puts "Please pick number:
-            1) Weather Report
-            2) Temperature
-            3) Weather Condition
-            4) Wind Speed
-            5) Sunrise/Sunset"
+      puts "
+1) Weather Report
+2) Temperature
+3) Weather Condition
+4) Wind Speed
+5) Sunrise/Sunset"
+    else 
+      puts "Stay dry!"
+    end 
+  end
+  
+    def choose            
+      choice = nil
+    while choice != "exit"
+      puts "Choose a number or input exit to leave."
             
             choice = gets.chomp
             
@@ -25,14 +39,13 @@ class Umbrella::CLI
             when "4"
               puts 'wind'
             when "5"
-              puts 'ss '
+              puts 'ss'
             when "exit"
               puts "Stay dry!"
             else
               puts "Did not understand that commmand. Please choose a number 1-5 or type 'exit' to quit."
-            end   
-    else 
-      puts "Stay dry!"
+            end  
+        end
   end 
   
 end 
