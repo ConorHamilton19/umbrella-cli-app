74f69059525824f3eb5a96cd09dce9b825d53723
Conor Hamilton <conhamil19@gmail.com>
2018-06-08 04:20:30 +0000

    environment set up

--- Umbrella/lib/Umbrella.rb
+++ Umbrella/lib/Umbrella.rb
@@ -1,4 +1,7 @@
-require "Umbrella/version"
+require_relative "./Umbrella/version"
+require_relative "./Umbrella/CLI"
+require_relative "./Umbrella/weather"
+require_relative "./Umbrella/weather_scraper"
 
 module Umbrella
   # Your code goes here...
