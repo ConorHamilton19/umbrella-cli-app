211196596e6809a18bb9c963f54af4a8f8c543c5
Conor Hamilton <conhamil19@gmail.com>
2018-06-12 02:49:41 +0000

    Scrape done

--- Umbrella/lib/Umbrella/weather.rb
+++ Umbrella/lib/Umbrella/weather.rb
@@ -1,19 +1,17 @@
 class Umbrella::Weather
   attr_accessor :temperature, :wind, :sunrise_sunset, :weather_condition, :rain_perc
   
-  def initialize
-    weather_setter
-  end 
-  
   def self.weather_setter
-    @temperature = "scraped info"
-    @weather_condition = "scraped"
-    @wind = "scraped"
-    @sunrise_sunset = "scraped"
-    @rain_perc = "scraped"
-  end 
+    
+    doc = Nokogiri::HTML(open("https://weather.com/weather/today/l/Chicago+IL+USIL0225:1:US"))
+    
+    rain = self.new 
+    
+    rain.temperature = doc.search("div.today_nowcard-temp").text
+    rain.weather_condition = doc.search("div.today_nowcard-phrase").text 
+    rain.wind = doc.search("div.today_nowcard-sidecar").search("td")[0].text
+    rain.sunrise = doc.search("span.wx-dsxdate")[0].text
+    rain.sunset = doc.search("span.wx-dsxdate")[1].text
+    rain.rain_perc = doc.search("span.precip-val").first.text
   
-  def self.weather_scrape
-    doc = Nokogiri::HTML(open())
   end 
-end 
